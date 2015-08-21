7428b2452cd567b823208ab6088a319d4855d785
sanayaCorp <revisoft783@gmail.com>
2015-08-21 08:00:27 +0700

    Research Traccar-UI, change EventProperties for DeviceEventView.java

--- src/main/java/org/traccar/web/client/model/EventProperties.java
+++ src/main/java/org/traccar/web/client/model/EventProperties.java
@@ -5,11 +5,16 @@
  */
 package org.traccar.web.client.model;
 
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.editor.client.Editor;
 import com.sencha.gxt.core.client.ValueProvider;
+import com.sencha.gxt.data.shared.LabelProvider;
 import com.sencha.gxt.data.shared.ModelKeyProvider;
 import com.sencha.gxt.data.shared.PropertyAccess;
 import java.util.Date;
+import org.traccar.web.client.i18n.Messages;
 import org.traccar.web.shared.model.DeviceEvent;
+import org.traccar.web.shared.model.DeviceEventType;
 
 /**
  *
@@ -20,7 +25,18 @@ public interface EventProperties extends PropertyAccess<DeviceEvent> {
     
     ValueProvider<DeviceEvent, Date> time();
     
-    ValueProvider<DeviceEvent, String> name();
+    ValueProvider<DeviceEvent, String> name(); 
     
-   // ValueProvider<DeviceEvent, String> type();
+    ValueProvider<DeviceEvent, DeviceEventType> type();
+    
+    
+//    ValueProvider<DeviceEvent, String> type();
+    public static class DeviceEventTypeLabelProvider implements LabelProvider<DeviceEventType> {
+        final Messages i18n = GWT.create(Messages.class);
+        
+        @Override
+        public String getLabel(DeviceEventType type) {
+            return i18n.deviceEventType(type);
+        }
+    }
 }
